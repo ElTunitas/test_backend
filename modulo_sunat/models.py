85c6858ac9800b6f76edc99050dd95d75bf6a779
unknown <vilcaangel373@gmail.com>
2025-11-24 18:28:26 -0500

    quinta subida

--- modulo_sunat/models.py
+++ modulo_sunat/models.py
@@ -1,13 +1,2 @@
 from django.db import models
 git pull origin master 
-
-XDXDXDD
-holi
-tercera prueba
-
-terminal / new terminal / git bash
-
-git add .
-git commit -m "nomnbre cualquiera"
-git push origin master
-ggg
