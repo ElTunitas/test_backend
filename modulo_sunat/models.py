3ab2b42812dfe8b014f2e9fcec76474b89d693e0
unknown <vilcaangel373@gmail.com>
2025-11-24 17:49:36 -0500

    tercera subida

--- modulo_sunat/models.py
+++ modulo_sunat/models.py
@@ -2,3 +2,11 @@ from django.db import models
 git pull origin master 
 
 XDXDXDD
+holi
+tercera prueba
+
+terminal / new terminal / git bash
+
+git add .
+git commit -m "nomnbre cualquiera"
+git push origin master
